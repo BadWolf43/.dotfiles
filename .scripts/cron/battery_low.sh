c73dc05dccd8f09c1c8b78fb4fcb066b095178d8
Ryan Nader <Rnader43@gmail.com>
2019-04-18 21:32:52 -0400

    Added battery_low.sh

---
+++ .scripts/cron/battery_low.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Notify me with notify-send if my battery is below 20%.
+# set with crontab -e
+
+[ "$(cat /sys/class/power_supply/BAT0/status)" = "Charging" ] && exit
+[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt 20 ] &&
+#export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus &&
+notify-send -u critical "Battery critically low."
